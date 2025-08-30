8b12a328a4a7fa397572f131a829450122785cdd
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-08-30 11:55:09 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; // import library Scanner to get input from user
+
+public class Main {
+  public static void main(String[] args) {
+    int n =0;
+    for (int i =1 ; i<=10; i++ ) { // keep on looping until i = 10
+      n += i; // add i to n
+    }
+    System.out.println(n); // print the result
+  }
+}
