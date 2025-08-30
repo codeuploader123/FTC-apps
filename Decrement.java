fed4839aef4ed7924b9cf93d418d49206ce61d89
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-08-30 12:05:47 +0530

    Create Decrement.java

---
+++ Decrement.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; // import library Scanner to get input from user
+
+public class Main {
+  public static void main(String[] args) {
+    
+    int i = 10; // initialize i to 10
+    do {
+      System.out.println(i); // ask user to enter a number
+      i--; // decrement i by 1
+    }
+      while (i >=1 ); // check if i is greater than 1
+
+  }
+}
