3bde24a8957629fab1f7e3e03e1130bff501b25f
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-08-31 12:44:24 +0530

    Create functionsNumbers.java

---
+++ functionsNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; // import library Scanner to get input from user
+
+public class Main {
+  public static void main(String[] args) {
+    printNumbers();
+  } 
+  public static void printNumbers()
+    {
+      for (int i = 1; i <= 10; i++){
+        System.out.println(i);
+      }
+    }
+}
+
+
