20782a25138ae2e825f0d82f8d60d5858ef4eb2b
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-08-31 12:31:29 +0530

    Create function1.java

---
+++ function1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; // import library Scanner to get input from user
+
+public class Main {
+  public static void main(String[] args) {
+    displayInfo("Ahaan", 9 , "Maths");
+  } 
+  public static void displayInfo(String name, int grade, String subject)
+    {
+      System.out.println("Student name: " + name + "\nGrade: " + grade + "\nSubject: " + subject);
+    }
+}
+
+
