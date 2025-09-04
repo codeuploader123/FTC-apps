206c4d17a0fb57a1494d4337edf8befe8b5b9fca
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-09-04 18:55:07 +0530

    Create mathVarious_back.java

---
+++ mathVarious_back.java
@@ -0,0 +1,9 @@
+public class multiply {
+  public static void printTable(int n){
+    for(int i = 1; i <= 10; i++){ // loop from 1 to 10
+      System.out.println(n + " * " + i + " = " + n*i); // print the multiplication table
+    }
+  }
+}
+
+
