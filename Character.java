17cb83ca49ffe055194df041cd998e911935340e
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-09-14 11:20:14 +0530

    Update Character.java

--- Character.java
+++ Character.java
@@ -5,8 +5,8 @@ public class Main {
     Scanner sc = new Scanner(System.in); // create a Scanner object
     System.out.print("Enter a word: ");
     String word = sc.nextLine(); // read user input
-    for (int i = 0; i < word.length(); i++){
-      System.out.println(word.charAt(i));
+    for (int i = 0; i < word.length(); i++){ // check index for all letters
+      System.out.println(word.charAt(i)); // print the character on a new line
     }
     }
 }
