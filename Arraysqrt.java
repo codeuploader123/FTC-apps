e419f386f15b8bac661eabf30b85191305bd2e29
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-09-20 11:24:43 +0530

    Create Arraysqrt.java

---
+++ Arraysqrt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; // import library Scanner to get input from user
+import java.lang.Math.*;
+
+public class Main {
+  public static void main(String[] args) {
+    double[] numbers = {1,4,9,16};
+    double number = numbers[1];
+    double finals = Math.sqrt(number);
+    System.out.println(finals);
+  }
+}
