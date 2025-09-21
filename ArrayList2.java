6e6f02cb1946fa2d19d21939c608cc0d189a1b55
codeuploader123 <ahaan.chhaparwal@gmail.com>
2025-09-21 12:00:18 +0530

    Create ArrayList2.java

---
+++ ArrayList2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> name = new ArrayList<>(); // create an ArrayList 
+    name.add(10); // add numbers  
+    name.add(20);
+    name.add(30);
+    name.add(40);
+    name.remove(2);
+    System.out.println(name.size()); // print the size of the ArrayList
+    System.out.println(name); // print the ArrayList
+      
+  }
+}
